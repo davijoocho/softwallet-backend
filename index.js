7aab15e7dcb8030a40e3a9e771e2c16c681107db
David Choi <choidavid478@gmail.com>
2020-07-13 20:21:55 -0700

    Add middleware for access token route

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const cors = require('cors');
 const signInRoute = require('./routes/signin.js'); 
 const signUpRoute = require('./routes/signup.js');
 const dashboardRoute = require('./routes/dashboard.js');
+const accessTokenRoute = require('./routes/access-token.js');
+
 
 
 
 app.use(express.json());
 app.use(cors());
 
+
+
+//app.get('/get_balance')
+//Link to get access token
+//After user has access token just use that to get the balance everytime 
+
+
+app.use('/get_access_token', accessTokenRoute);
 
 //Middleware for Sign-Up Routes
 
@@ -22,6 +32,7 @@ app.use('/signin', signInRoute);
 //Middleware for Dashbaord Routes
 
 app.use('/dashboard', dashboardRoute);
+
 
 
 
